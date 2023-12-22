30b2394c5408ca79c90e836461328d98d703e0c5
Robin Zhu <robin_zhwb@163.com>
2023-12-23 07:08:26 +0800

    enhance UX

--- js/main.js
+++ js/main.js
@@ -35,6 +35,10 @@ $(document).ready(() => {
     });
 
     $("#shortcut_random_yao").click(function () {
+        if (getHighlightedYaoArr().length > 0) {
+            return;
+        }
+
         let randomYao = frequentlyUsedYaoDict[getRandomInt(frequentlyUsedYaoDict.length)];
         $("#search_input").val(randomYao);
         analyze();
@@ -617,30 +621,4 @@ function getRandomInt(max) {
 function precisionRound(number, precision) {
     let factor = Math.pow(10, precision);
     return Math.round(number * factor) / factor;
-}
-
-function createCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
 }
